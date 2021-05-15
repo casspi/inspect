5debd9747f76f664a8afb3c4ad2b55e80a19785b
livew2006 <livew2006@126.com>
2021-05-15 23:17:21 +0800

    用户登录

--- admin-ui/src/views/login.vue
+++ admin-ui/src/views/login.vue
@@ -66,7 +66,7 @@ export default {
       cookiePassword: "",
       loginForm: {
         username: "admin",
-        password: "admin123",
+        password: "888888",
         rememberMe: false,
         code: "",
         uuid: ""
