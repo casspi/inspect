0b49ed43b46ce09e1bc9804f69beadaccfa02cfd
darren <livew2006@126.com>
2022-06-07 18:54:21 +0800

    详情

---
+++ admin-ui/src/views/base/inspectionItem/detail.vue
@@ -0,0 +1,340 @@
+<template>
+  <div class="app-container">
+   
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="检验项目名称" prop="itemName">
+          <el-input v-model="form.itemName" placeholder="请输入检验项目名称" />
+        </el-form-item>
+        <el-form-item label="检验项目分类" prop="itemClassify">
+          <el-select v-model="form.itemClassify" placeholder="请输入检验项目分类" >
+            <el-option
+              v-for="dict in itemClassifyOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="样本类型" prop="sampleType">
+          <el-select v-model="form.sampleType" placeholder="请选择样本类型" >
+            <el-option
+              v-for="dict in sampleTypeOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="单位" prop="itemUnit">
+          <el-input v-model="form.itemUnit" placeholder="请输入单位" />
+        </el-form-item>
+        <el-form-item label="价格" prop="amount">
+          <el-input v-model="form.amount" placeholder="请输入价格" />
+        </el-form-item>
+        <!-- <el-form-item label="折扣百分比" prop="discountPercent">
+          <el-input v-model="form.discountPercent" placeholder="请输入折扣百分比" />
+        </el-form-item> -->
+        <el-form-item label="折扣金额" prop="discountAmount">
+          <el-input v-model="form.discountAmount" placeholder="请输入折扣金额" />
+        </el-form-item>
+        <el-form-item label="检验项目介绍" prop="summary">
+          <el-input v-model="form.summary" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+        <el-form-item label="检验所" prop="inspectionOfficeId">
+          <el-select v-model="form.inspectionOfficeId" placeholder="请选择检验所" @change="inspectionOfficeItemChange(form.inspectionOfficeId,null)">
+            <el-option
+              v-for="dict in inspectionOfficeList"
+              :key="dict.id"
+              :label="dict.officeName"
+              :value="dict.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="检验所项目" prop="inspectionOfficeItemId">
+           <el-select v-model="form.inspectionOfficeItemId" placeholder="请选择检验所项目" >
+            <el-option
+              v-for="dict in inspectionOfficeItemList"
+              :key="dict.id"
+              :label="dict.itemName"
+              :value="dict.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+       <el-form-item label="状态">
+          <el-radio-group v-model="form.status">
+                <el-radio
+                  v-for="dict in statusOptions"
+                  :key="dict.dictValue"
+                  :label="dict.dictValue"
+                >{{dict.dictLabel}}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+import { listInspectionItem, getInspectionItem, delInspectionItem, addInspectionItem, updateInspectionItem, exportInspectionItem,changeStatus } from "@/api/base/inspectionItem";
+import { getInspectionOfficeList } from "@/api/base/inspectionOffice";
+import { getListByInspectionOfficeId } from "@/api/base/inspectionOfficeItem";
+import { checkAmount }from "@/utils/index";
+
+export default {
+  name: "InspectionItem",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 状态数据字典
+      statusOptions: [],
+      // 总条数
+      total: 0,
+      // 检验项目信息表格数据
+      inspectionItemList: [],
+      //检验所
+      inspectionOfficeList: [],
+      //检验所项目
+      inspectionOfficeItemList: [],
+      //样本类型
+      sampleTypeOptions: [],
+
+    //样本分类
+      itemClassifyOptions: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        itemName: null,
+        itemClassify: null,
+        sampleType: null,
+        itemUnit: null,
+        amount: null,
+        discountPercent: null,
+        discountAmount: null,
+        summary: null,
+        inspectionOfficeId: null,
+        inspectionOfficeItemId: null,
+        status: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        status: [
+          { required: true, message: "状态不能为空", trigger: "blur" }
+        ],
+        itemName: [
+          { required: true, message: "检验项目不能为空", trigger: "blur" }
+        ],
+        itemClassify: [
+          { required: true, message: "项目分类不能为空", trigger: "blur" }
+        ],
+        sampleType: [
+          { required: true, message: "样本类型不能为空", trigger: "blur" }
+        ],
+        amount: [
+          { required: true, message: "价格不能为空", trigger: "blur" },
+          { validator: checkAmount, trigger: "blur" }
+        ],
+        discountAmount: [
+          { required: true, message: "折扣金额不能为空", trigger: "blur" },
+           { validator: checkAmount, trigger: "blur" }
+        ],
+        inspectionOfficeId: [
+          { required: true, message: "检验所不能为空", trigger: "blur" }
+        ],
+        inspectionOfficeItemId: [
+          { required: true, message: "检验所项目不能为空", trigger: "blur" }
+        ],
+      }
+    };
+  },
+  created() {
+    this.getList();
+    this.inspectionOfficeData();
+    this.getDicts("sys_normal_disable").then(response => {
+      this.statusOptions = response.data;
+    });
+    this.getDicts("inspect_item_classify").then(response => {
+      this.itemClassifyOptions = response.data;
+    });
+    this.getDicts("inspect_sample_type").then(response => {
+      this.sampleTypeOptions = response.data;
+      console.log(this.sampleTypeOptions);
+    });
+  },
+  methods: {
+    /** 查询检验项目信息列表 */
+    getList() {
+      this.loading = true;
+      listInspectionItem(this.queryParams).then(response => {
+        this.inspectionItemList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        itemName: null,
+        itemClassify: null,
+        sampleType: null,
+        itemUnit: null,
+        amount: null,
+        discountPercent: null,
+        discountAmount: null,
+        summary: null,
+        inspectionOfficeId: null,
+        inspectionOfficeItemId: null,
+        status: "0",
+        delFlag: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+        remark: null
+      };
+      this.resetForm("form");
+    },
+            // 状态修改
+    handleStatusChange(row) {
+      let text = row.status === "0" ? "启用" : "停用";
+      this.$confirm('确认要' + text + '<' + row.itemName + '>项目吗?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return changeStatus(row.id, row.status);
+        }).then(() => {
+          this.msgSuccess(text + "成功");
+        }).catch(function() {
+          row.status = row.status === "0" ? "1" : "0";
+        });
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加检验项目信息";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      let _this = this;
+      const id = row.id || this.ids
+      let officeId = '';
+      getInspectionItem(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改检验项目信息";
+        _this.inspectionOfficeItemChange(response.data.inspectionOfficeId,this.form.inspectionOfficeItemId);
+      });
+    },
+
+    /** 检验所列表 */
+    inspectionOfficeData() {
+      this.inspectionOfficeItemList = [];
+      getInspectionOfficeList().then(response => {
+        this.inspectionOfficeList = response.data;
+      });
+    },
+        /** 修改按钮操作 */
+    inspectionOfficeItemChange(id,itemId) {
+      let _this= this;
+      if(itemId==null){
+        this.form.inspectionOfficeItemId = ''
+      }
+      getListByInspectionOfficeId(id).then(response => {
+        _this.inspectionOfficeItemList = response.data;
+        if(itemId!=null){
+        _this.inspectionOfficeItemId=itemId;
+        }
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateInspectionItem(this.form).then(response => {
+              this.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addInspectionItem(this.form).then(response => {
+              this.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除检验项目信息编号为"' + ids + '"的数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return delInspectionItem(ids);
+        }).then(() => {
+          this.getList();
+          this.msgSuccess("删除成功");
+        })
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有检验项目信息数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return exportInspectionItem(queryParams);
+        }).then(response => {
+          this.download(response.data);
+        })
+    }
+  }
+};
+</script>
