569f5d7b1007783117b2bc3563416fd120cd3dd4
智兵 <369642339@qq.com>
2022-06-12 18:42:09 +0800

    22

--- zhjy/src/views/CreateOrder.vue
+++ zhjy/src/views/CreateOrder.vue
@@ -185,7 +185,7 @@ export default {
     }).then(res => {
       const {appId, timestamp, nonceStr, signature} = res.data
       window.wx.config({
-        debug: true, // 开启调试模式,调用的所有 api 的返回值会在客户端 alert 出来，若要查看传入的参数，可以在 pc 端打开，参数信息会通过 log 打出，仅在 pc 端时才会打印。
+        debug: process.env.NODE_ENV === 'development'? true: false, // 开启调试模式,调用的所有 api 的返回值会在客户端 alert 出来，若要查看传入的参数，可以在 pc 端打开，参数信息会通过 log 打出，仅在 pc 端时才会打印。
         appId, // 必填，公众号的唯一标识
         timestamp, // 必填，生成签名的时间戳
         nonceStr, // 必填，生成签名的随机串
