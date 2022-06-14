76baf814dace46a41a38e0f215a353a9bde97ed0
darren <livew2006@126.com>
2022-06-14 23:03:36 +0800

    错误返回  信息

--- zhjy/src/utils/axios.js
+++ zhjy/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { Toast } from 'vant'
 import router from '../router'
 console.log(process.env)
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'https://gzh.huichangyx.com/prod-api/' : 'https://gzh.huichangyx.com/prod-api/'
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'https://96166.xiaomy.net' : 'https://gzh.huichangyx.com/prod-api/'
 axios.defaults.withCredentials = true
 axios.defaults.timeout = 5000;
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
@@ -30,12 +30,18 @@ axios.interceptors.response.use((res) => {
       return Promise.reject(res.data)
     }
     // if (res.data.msg) Toast.fail(res.data.msg)
-    return Promise.reject(res.data)
+    return res.data;
   }
 
   return res.data
 },error => {
-  return Promise.reject(error)
+  console.log("aaaaaa   ",error);
+  let t={
+    'code':500,
+    'msg':'系统异常，请稍候！'
+  }
+  console.log(t);
+  return t;
 
 })
 
